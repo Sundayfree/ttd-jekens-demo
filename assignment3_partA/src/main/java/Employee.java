5e4dbf8f8842a86c5a49138418c00403a0d2cd08
Sundayfree <978591397@qq.com>
2019-09-18 21:49:26 +1200

    done almost;

--- assignment3_partA/src/main/java/Employee.java
+++ assignment3_partA/src/main/java/Employee.java
@@ -4,26 +4,7 @@
  * Date: 2019-09-18 15:24
  */
 public class Employee {
-
-    private String eId;
-    private  String name;
     private  double salary;
-
-    public String geteId() {
-        return eId;
-    }
-
-    public void seteId(String eId) {
-        this.eId = eId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public double getSalary() {
         return salary;
--- assignment3_partA/src/test/java/AccountServiceTest.java
+++ assignment3_partA/src/test/java/AccountServiceTest.java
@@ -3,6 +3,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.mockito.stubbing.OngoingStubbing;
 
 /**
@@ -17,14 +19,20 @@ public class AccountServiceTest {
     double expected;
     double actual;
     double mockData;
+    Employee employee;
     @Before
     public void setUp() throws  Exception{
         account= new AccountServiceImpl ();
         mock = Mockito.mock (IAccountService.class);
         mockData = 10.0;
         Mockito.when (mock.computeTax (mockData)).thenReturn (expected);
-        account.setAccountService (mock);
+        account.setAccountService (this.mock);
         actual= account.computeTax (mockData);
+        employee = Mockito.mock (Employee.class);
+        employee.setSalary (20.0);
+        Mockito.when (mock.weeklySalary (employee)).thenReturn (expected);
+        actual = account.weeklySalary (employee);
+
     }
     @Test
     public void computeTaxTest(){
@@ -33,7 +41,9 @@ public class AccountServiceTest {
 
     @Test
     public void weeklySalaryTest(){
-
+        Assert.assertEquals (expected,actual,0.1);
+    }
+    @Test void fortnightSalaryTest(){
 
     }
 
